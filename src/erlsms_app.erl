f0cc5101fd590d6b00d98bf0be516859095d4793
lamuswawir <muswawir25@gmail.com>
2022-02-10 11:27:44 +0300

    fix a few changes in the app

--- src/erlsms_app.erl
+++ src/erlsms_app.erl
@@ -11,9 +11,9 @@
 -export([start/2, stop/1]).
 -export([send/2]).
 
--define(Username, application:get_env(sms, username)).
--define(Password, application:get_env(sms, password)).
--define(Sender, application:get_env(sms, sender)).
+-define(Username, begin {ok, Username} = application:get_env(sms, username), Username end).
+-define(Password, begin {ok, Password} = application:get_env(sms, password), Password end).
+-define(Sender, begin {ok, Sender} = application:get_env(sms, sender),  Sender end).
 
 start(_StartType, _StartArgs) ->
     inets:start(),
@@ -23,7 +23,8 @@ stop(_State) ->
     ok.
 
 send(Number, Message) ->
-    Query = uri_string:compose_query([{"number", Number}, {"message", Message}, {"username", ?Username}, {"password", ?Password}, {"sender", ?Sender}]),
+    PlusLessNumber = lists:append( string:replace(Number, "+", "") ),
+    Query = uri_string:compose_query([{"number", PlusLessNumber}, {"message", Message}, {"username", ?Username}, {"password", ?Password}, {"sender", ?Sender}]),
     URI = "http://www.egosms.co/api/v1/plain/?"++Query,
     httpc:request(URI).
 
